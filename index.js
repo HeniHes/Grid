417c43b6816949f156fcc6bfd2077d3c5fe1b948
HeniHes <heni.es.paypal@gmail.com>
2024-06-02 18:11:30 +0100

    dashboard

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-
-
 // console.log(Pi);
 
 // const area= getArea(10);
 
 // console.log(`${area} cm`);
-
-
 
 // function fun1(callback){
 //   setTimeout(() => {
@@ -15,32 +11,47 @@
 //   }, 3000);
 // }
 
-
-
-// function fun2(){
-//   console.log("task2");
-//   console.log("Task3");
+// function fun2(fn){
+//   fn(1,2,3);
 // }
 
-// function walkDog(){
-//   return new Promise((resolve,reject)=>{
-//     setTimeout(()=>{
-//       resolve("you take out the trash");
-//     },500);
-//   });
+// function getDog(){
+//  fun2((qsD,b,c)=> console.log(a +b+c) )
 // }
+// getDog();
 
-// function cleanKitchen(){
-//   return new Promise((resolve,reject)=>{
-//     setTimeout(()=>{
-// resolve("you clean the kitchen")
-//     },1500)
-//   })
-// }
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("you take out the trash");
+    }, 500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("you clean the kitchen");
+    }, 1500);
+  });
+}
+
+const test = () => {
+  const test2 = () => console.log(`test`);
+  return { test2 };
+};
+
+test().test2();
 // walkDog().then(value=>{console.log(value); return cleanKitchen()}).then(value=>{console.log(value)})
 
+async function rami() {
+  const res = await walkDog();
+  console.log(res);
+  const res2 = await cleanKitchen();
+  console.log(res2);
+}
 
-
+rami();
 
 // let p = new Promise((resolve,reject)=>{
 //   let a = 2;
@@ -50,7 +61,7 @@
 //   else{
 //     reject("Failed");
 //   }},1000)
-  
+
 // })
 
 // let c= new Promise((resolve,reject)=>{
@@ -62,7 +73,7 @@
 //     else{
 //       reject("Failed")
 //     }
-    
+
 //   },1000)
 // })
 
@@ -112,26 +123,24 @@
 //     console.error(error);
 //   }
 // }
-// async function fetchData(){
-
-try{
-  const pokemanName= document.getElementById("pokemanName").value.toLowerCase();
-  const respone= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemanName}`)
-  if (!respone.ok){
-    throw new Error("could not fetch Data");
+async function fetchData() {
+  try {
+    const pokemanName = document
+      .getElementById("pokemanName")
+      .value.toLowerCase();
+    const respone = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemanName}`
+    );
+    if (!respone.ok) {
+      throw new Error("could not fetch Data");
+    }
+    const data = await respone.json();
+    console.log(data);
+    const pokemonSprite = data.sprites.front_default;
+    const imgPokemon = document.getElementById("pokemonSprite");
+    imgPokemon.src = pokemonSprite;
+    imgPokemon.style.display = "block";
+  } catch {
+    throw console.error("error");
   }
-  const data= await respone.json();
-  console.log(data);
-  const pokemonSprite= data.sprites.front_default;
-  const imgPokemon= document.getElementById("pokemonSprite");
-  imgPokemon.src=pokemonSprite;
-  imgPokemon.style.display="block";
-}
-catch{
-  throw console.error("error");
-}
-}
-
-async function fetchWeatherData(){
-
 }
